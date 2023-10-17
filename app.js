31fbb28e173240fb11a62a96d1d80e3213b91f5d
JMCA2805 <josemauricio2805@gmail.com>
2023-10-17 15:09:46 -0400

    Configuración del puerto y del Swagger

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const { swaggerDocs } = require("./swagger");
 
-var indexRouter = require('./src/routes/routes');
-
+var indexRouter = require("./src/routes/routes");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+
+app.listen(4000, () => {
+  console.log(`[Running] - PORT: 4000`);
+  console.log("[Link]    " + "http://localhost:4000");
+  swaggerDocs(app, 4000);
+});
 
 module.exports = app;
