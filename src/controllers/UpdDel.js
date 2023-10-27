d5dfb5579cf51a8bea9d355121d12aeda32280be
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-10-27 10:25:08 -0400

    Editar

--- src/controllers/UpdDel.js
+++ src/controllers/UpdDel.js
@@ -4,29 +4,30 @@ const fs = require('fs'); // importa el módulo fs para acceder al sistema de ar
 
 const itemUpdate = async (req, res) => {
   try {
-    const titulo = req.params.titulo;
-
+    const itemId = req.params.id; // Obtén el ID del item que se desea editar
     const updatedData = {
+      titulo: req.body.titulo,
+      imagen: req.file.path,
       marca: req.body.marca,
-      modelo: req.body.modelo,
-      cantidad: req.body.cantidad,
+      modelo:req.body.modelo,
+      cantidad:req.body.cantidad,
       precio_adquisicion: req.body.precio_adquisicion,
       fecha_adquisicion: req.body.fecha_adquisicion
     };
 
-    const result = await model.findOneAndUpdate({ titulo: titulo }, updatedData);
+    const updatedItem = await model.findByIdAndUpdate(itemId, updatedData, { new: true });
+    // Encuentra y actualiza el item en la base de datos, y regresa el item actualizado
 
-    if (result) {
+    if (updatedItem) {
       res.status(200).json({ message: "Item actualizado exitosamente", status: 200 });
     } else {
-      res.status(400).json({ message: "No se encontró ningún item con el título proporcionado", status: 400 });
+      res.status(404).json({ message: "No se encontró el item", status: 404 });
     }
   } catch (error) {
     console.log('Error', error);
-    res.status(500).json({ message: "Error al intentar actualizar el item", status: 500 });
+    res.status(500).json({ message: "Error al intentar editar el item", status: 500 });
   }
 };
-
 const itemDelete = async (req, res) => {
   const id = req.params.id;
  
