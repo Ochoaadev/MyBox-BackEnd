1b8f1ec441fcc791bb86c3c8e6b08bc31494a4fb
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-10-30 21:37:55 -0400

    Update conexion.js

--- src/config/conexion.js
+++ src/config/conexion.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 //Configuracion de la base de datos
 
 const mongoose = require('mongoose');
@@ -6,17 +7,23 @@ const mongoose = require('mongoose');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@mybox.n6jacmb.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
 module.exports = () => {
-  const Connection = () => {
-    mongoose
-      .connect(uri, 
-        {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        })
-      .then(() => console.log('Connected to database'))
 
-      .catch(e => console.log('Not connected', e));
+  const handleError = (e) => {
+     console.error(`Ocurrió un error al conectar con la base de datos: ${e.message}`);
+     process.exit(1);
   };
-
+ 
+  const Connection = () => {
+     mongoose
+       .connect(uri, 
+         {
+             useNewUrlParser: true, 
+             useUnifiedTopology: true
+         })
+       .then(() => console.log('Conectado a la base de datos correctamente'))
+ 
+       .catch(handleError);
+  };
+ 
   Connection();
-};
+ };
