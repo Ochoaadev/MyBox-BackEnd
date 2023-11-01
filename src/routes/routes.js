2d720a61f4fe9b6f8ac6de52b89126bd3e946152
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-10-31 23:11:41 -0400

    Agrregada la rua de Categorias

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,12 @@ const { itemsSolic, itemList } = require("../controllers/AggProduct");
 const { itemUpdate, itemDelete } = require('../controllers/UpdDel')
 
 const {FilterProducts } = require('../controllers/Filter_Products')
+
+
+//-----------Categorias--------------
+const { cat_List, cat_add, del_cat } = require("../controllers/Categoria_Get_Post_Del");
+
+
 // ----------- Rutas ---------------
 
 //Obtener
@@ -24,5 +30,11 @@ router.put('/items/:id', itemUpdate);
 router.delete('/item/:id', itemDelete);
 
 router.get('/FilterProducts/:titulo', FilterProducts);
+
+
+//------Categorias
+router.get("/Listar_categorias", cat_List);
+router.post("/Agregar_categorias", cat_add);
+router.delete("/Eliminar_categorias/:id", del_cat);
 
 module.exports = router;
