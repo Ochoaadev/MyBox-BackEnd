21dda8b3ce118fd73166beb681493ba064051c4f
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-11-04 11:06:33 -0400

    Devolviendo id al Cliente

--- src/controllers/Users/Login-Register.js
+++ src/controllers/Users/Login-Register.js
@@ -1,44 +1,78 @@
-const model = require('../../models/users');
-const {Encrypt,Compare} = require('../../middlewares/bcrypt')
-const {GenerarToken} = require('../../middlewares/JWT')
+const model = require("../../models/users");
+const { Encrypt, Compare } = require("../../middlewares/bcrypt");
+const { GenerarToken } = require("../../middlewares/JWT");
 
 const register = async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   let rol = "";
   if (req.body.rol == "Admin") {
     rol = "Admin";
-
-  }else{
+  } else {
     rol = "User";
   }
   try {
-    const { name, lastname, email, password, phone, username, gender } = req.body;
-    if (!name || !email || !password || !lastname || !phone || !username || !gender) {
-      return res.status(400).json({ message: "Faltan datos", status: 400});
+    const { name, lastname, email, password, phone, username, gender } =
+      req.body;
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !lastname ||
+      !phone ||
+      !username ||
+      !gender
+    ) {
+      return res.status(400).json({ message: "Faltan datos", status: 400 });
     }
 
     const hash = await Encrypt(password);
 
-    const user = { name, lastname, email, 'password':hash,phone,rol,username, gender}
-    const data = new model(user)
-    const payload = {
-      username,
+    const user = {
+      name,
+      lastname,
       email,
-      rol
-    }
-    const token = GenerarToken(payload);
+      password: hash,
+      phone,
+      rol,
+      username,
+      gender,
+    };
+    const data = new model(user);
 
-    console.log(hash,token,user)
     try {
       //Se procede a guardar
       await data.save();
       //En tal caso todo haya salido bien
-      res.status(200).json({payload, message: "Registro Exitoso",token, status: 200});
+
+      const datas = async (dato) => {
+        const documents = await model.find({ username: username });
+        return documents;
+      };
+
+      const getdata = await datas(username);
+
+      const payload = {
+        userId: getdata[0]._id,
+        username,
+        email,
+        rol,
+      };
+      const token = GenerarToken(payload);
+
+      if (!data) {
+        return res
+          .status(404)
+          .json({ message: "Usuario o contraseña incorrectos", status: 404 });
+      }
+
+      res
+        .status(200)
+        .json({ payload, message: "Registro Exitoso", token, status: 200 });
     } catch (error) {
       //Caso contrario
-      console.log('Error', error);
-      res.status(500).json({message: "Error al registrarse", status: 500});
-    }    
+      console.log("Error", error);
+      res.status(500).json({ message: "Error al registrarse", status: 500 });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -48,37 +82,50 @@ const login = async (req, res) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
-      return res.status(400).json({ message: "Faltan datos", status: 400});
+      return res.status(400).json({ message: "Faltan datos", status: 400 });
     }
 
     const datas = async (dato) => {
-      const documents = await model.find({"username": dato})
+      const documents = await model.find({ username: dato });
       return documents;
-    }
-    
-    const data = await datas(username)
+    };
+
+    const data = await datas(username);
 
     if (!data) {
-       return res.status(404).json({ message: "Usuario o contraseña incorrectos", status: 404 });
+      return res
+        .status(404)
+        .json({ message: "Usuario o contraseña incorrectos", status: 404 });
     }
 
-    const user = { username, password }
+    const user = { username, password };
 
     const match = await Compare(user.password, data[0].password);
     if (!match) {
-      return res.status(401).json({ message: "Contraseña o Usuario incorrecto",status: 401});
+      return res
+        .status(401)
+        .json({ message: "Contraseña o Usuario incorrecto", status: 401 });
     }
     const payload = {
+      userId: data[0]._id,
       username: data[0].username,
       email: data[0].email,
       rol: data[0].rol,
-    }
+    };
     const token = await GenerarToken(payload);
 
-    res.status(200).json({ token, payload ,message: "Inicio de Sección Exitoso", status: 200 });
-
+    res.status(200).json({
+      token,
+      payload,
+      message: "Inicio de Sección Exitoso",
+      status: 200,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Contraseña o Usuario incorrecto", message1: error.message, status: 500 });
+    res.status(500).json({
+      message: "Contraseña o Usuario incorrecto",
+      message1: error.message,
+      status: 500,
+    });
   }
 };
 
