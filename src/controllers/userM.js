081e955653b7ddb2de72677f8e5bbe51f77bbf52
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-11-05 00:37:09 -0400

    Update userM.js

--- src/controllers/userM.js
+++ src/controllers/userM.js
@@ -1,26 +1,27 @@
 const model = require('../models/users')
 const bcrypt = require('bcrypt')
 
-//Obtener usuario
+//<<<<<<<<<<<<<<<<<<<<<<<Obtener usuario
+
 const GetUser = async (req, res) =>{
-    try {
-        const { id } = req.params; 
-        const user = await model.findById(id);
+   try {
+       const { id } = req.params; 
+       const user = await model.findById(id);
 
-        if (!user) {
-            return res.status(404).json({ message: "Usuario no encontrado", status: 404 });
-        }
+       if (!user) {
+           return res.status(404).json({ message: "Usuario no encontrado", status: 404 });
+       }
 
-        res.status(200).json(user);
-      } catch (error) {
-        console.log("Error:", error);
-        res
-          .status(500)
-          .json({ message: "Error al intentar listar los items", status: 500 });
-      }
+       res.status(200).json(user);
+     } catch (error) {
+       console.log("Error:", error);
+       res
+         .status(500)
+         .json({ message: "Error al intentar listar los items", status: 500 });
+     }
 }
 
-//Eliminar Usuario
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Eliminar Usuario
 
 const deleteUser = async (req, res) => {
     try {
@@ -31,38 +32,69 @@ const deleteUser = async (req, res) => {
     }
 };
 
-//Editar usuario
+//>>>>>>>>>>>>>>>>Editar usuario
+
 const editUser = async (req, res) => {
- try {
-    let user = await model.findById(req.params.id);
+   try {
+      let user = await model.findById(req.params.id);
+  
+      if (!user) {
+        return res.status(404).json({ msg: 'Usuario no encontrado' });
+      }
+  
+      user.name = req.body.name;
+      user.lastname = req.body.lastname
+      user.email = req.body.email;
+      user.phone = req.body.phone;
+      user.rol = req.body.rol;
+      user.username = req.body.username;
+      user.gender = req.body.gender;
+  
+      if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(req.body.password, salt);
+      }
+  
+      delete user.password;
+  
+      await user.save();
+  
+      res.status(200).json({ msg: 'Usuario Actualizado Correctamente' });
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Server error' });
+   }
+  };
 
-    if (!user) {
-      return res.status(404).json({ msg: 'Usuario no encontrado' });
-    }
+//<<<<<<<<<<<<<<<<<<<<<<<Actualizar Contraseña>>>>>>>>>>>>>>>>
 
-    user.name = req.body.name;
-    user.lastname = req.body.lastname
-    user.email = req.body.email;
-    user.phone = req.body.phone;
-    user.rol = req.body.rol;
-    user.username = req.body.username;
-    user.gender = req.body.gender;
+const updatePassword = async (req, res) => {
+   try {
+     const { id } = req.params;
+     const { oldPassword, newPassword } = req.body;
+ 
+     const user = await model.findById(id);
+     if (!user) {
+       return res.status(404).json({ message: "Usuario no encontrado", status: 404 });
+     }
+ 
+     const isMatch = await bcrypt.compare(oldPassword, user.password);
+     if (!isMatch) {
+       return res.status(400).json({ message: "Contraseña antigua incorrecta" });
+     }
+ 
+     const salt = await bcrypt.genSalt(10);
+     user.password = await bcrypt.hash(newPassword, salt);
+ 
+     await user.save();
+ 
+     res.status(200).json({ message: "Contraseña actualizada correctamente" });
+   } catch (error) {
+     console.log("Error:", error);
+     res.status(500).json({ message: "Error al intentar actualizar la contraseña", status: 500 });
+   }
+ };
+ 
 
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(req.body.password, salt);
-    }
+module.exports = {GetUser, deleteUser, editUser, updatePassword};
 
-    delete user.password;
-
-    await user.save();
-
-    res.status(200).json({ msg: 'Usuario Actualizado Correctamente' });
- } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: 'Server error' });
- }
-};
-   
-
-module.exports = {GetUser, deleteUser, editUser}
