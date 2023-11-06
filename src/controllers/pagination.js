7ea0320e55393a4e439f9035aa7100018def1d9e
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-11-05 23:41:50 -0400

    Implementando controlador de paginación

---
+++ src/controllers/pagination.js
@@ -0,0 +1,34 @@
+const model = require('../models/producto');
+
+function getAllProducts(req, res, next) {
+  const perPage = 20;
+  const page = req.query.page || 1;
+
+  model
+    .find({})
+    .skip((perPage * page) - perPage)
+    .limit(perPage)
+    .exec()
+    .then((products) => {
+      model.countDocuments()
+        .then((count) => {
+          const totalPages = Math.ceil(count / perPage);
+
+          res.json({
+            products,
+            current: page,
+            totalPages
+          });
+        })
+        .catch((err) => {
+          next(err);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+module.exports = {
+  getAllProducts
+};
